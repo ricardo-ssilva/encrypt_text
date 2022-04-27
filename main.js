47ff495a0023469a5c44d52bd29f3e6221a4e620
Ricardo Silva <ricardo.ssilva197@gmail.com>
2022-04-27 09:07:46 -0300

    apaga comentários

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-// A letra "e" é convertida para "enter"
-// A letra "i" é convertida para "imes"
-// A letra "a" é convertida para "ai"
-// A letra "o" é convertida para "ober"
-// A letra "u" é convertida para "ufat"
 
 const button =  document.querySelectorAll('.button')
 const input = document.querySelector('.text-bar input')
